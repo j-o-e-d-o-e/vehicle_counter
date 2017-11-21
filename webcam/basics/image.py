580b5b5ceb9a3e1a5bd3563b9b074ff101458a6c
joedoe <d_joe@gmx.net>
2017-11-21 02:43:36 +0100

    folder structure changed

--- webcam/basics/image.py
+++ webcam/basics/image.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("img/nice.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("img/image.jpg", cv2.IMREAD_GRAYSCALE)
 #img = cv2.imread("nice.jpg", cv2.BG)
 
 ##cv2.imshow("Nice",img)
