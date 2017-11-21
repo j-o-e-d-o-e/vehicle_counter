59d2671107528bc44683477ce11dbd5800013f48
j-o-e-d-o-e <d_joe@gmx.net>
2017-11-21 16:13:54 +0100

    videos added

--- picamera/basics/cam_recording.py
+++ picamera/basics/cam_recording.py
@@ -2,11 +2,9 @@ from picamera import PiCamera
 import time
 
 camera = PiCamera()
-camera.resolution = (100,100)
-camera.framerate=30
 camera.start_preview()
-camera.start_recording('/home/pi/Desktop/recording.h264')
-time.sleep(8)
+camera.start_recording('/home/pi/Desktop/opencv/vid/rec_16s.h264')
+time.sleep(16)
 camera.stop_recording()
 camera.stop_preview()
 
