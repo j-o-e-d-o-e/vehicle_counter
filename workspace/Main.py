1cdbc03a17f15856ced26374e2995f59e08309af
joedoe <d_joe@gmx.net>
2018-01-17 18:42:55 +0100

    multiple current centroids added

--- workspace/Main.py
+++ workspace/Main.py
@@ -2,35 +2,27 @@ import cv2
 import numpy as np
 import time
 
+import VehicleInfo
 from Vehicle import Vehicle
+from Centroid import Centroid
 
-KERNEL = np.ones((5, 5), np.uint8)
-LEARNING_RATE = 0.1
-MIN_RECTANGLE_SIZE = 150
-LINE_X = 1000
-LINE_START = (1000, 1000)
-LINE_END = (1000, 300)
-RED = [0, 0, 255]
-BLUE = [255, 0, 0]
-GREEN = [0, 255, 0]
-WHITE = [255, 255, 255]
-CIRCLE_RADIUS = 5
-THICKNESS = 10
-WINDOW_SIZE = 1150
-X_DEVIATION = 120
-Y_DEVIATION = 20
+KERNEL = np.ones((4, 4), np.uint8)  # 3, 4 or 5
+LEARNING_RATE = 0.1  # -1 or 0.1 to 0.5
+MIN_RECTANGLE_SIZE = 150  # 100 to 200
+WINDOW_SIZE = 970  # 600, 970 or 1150
 
-camera = cv2.VideoCapture("../vid/rec1_16s.h264")
-back_sub = cv2.createBackgroundSubtractorMOG2()
+image = []
+vehicles = []
+lastCentroids = []
+currentCentroids = []
 count = 0
 found = False
-last_centroid = []
-current_centroid = []
-vehicles = []
 
 
 def main():
-    global found
+    global image, found, lastCentroids, currentCentroids
+    camera = cv2.VideoCapture("../vid/rec1_8s.h264")
+    backSubtractor = cv2.createBackgroundSubtractorMOG2()  # () or (detectShadows=False)
     while True:
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
@@ -38,20 +30,45 @@ def main():
             time.sleep(3)
             found = False
         else:
-            return_value, image = camera.read()
-            if not return_value:
-                print("END OF VIDEO.")
-                for vehicle in vehicles:
-                    print(vehicle.get_all_centroids())
+            returnValue, image = camera.read()
+            if not returnValue:
                 break
-            foreground = getForeground(image)
-            detectVehicles(image, foreground)
 
-            if vehicles:
-                vehicles[-1].draw_vehicle_line(image, WHITE)
+            # get foreground containing the moving objects
+            foreground = getForeground(backSubtractor)
 
-            # draw crossing line:
-            cv2.line(image, LINE_START, LINE_END, RED, THICKNESS)
+            # get centroids of the moving objects from the foreground
+            lastCentroids = currentCentroids
+            currentCentroids = getCurrentCentroids(foreground)
+
+            # print and draw centroids
+            # for centroid in currentCentroids:
+            # centroid.draw(image)
+            # print(centroid.getXY())
+
+            # set direction of vehicles to better add centroids to these
+            for vehicle in [vehicle for vehicle in vehicles if not vehicle.getDirection()]:
+                vehicle.setDirection()
+
+            # delete found vehicles if they are finished
+            for vehicle in [vehicle for vehicle in vehicles if vehicle.isFound and not vehicle.isFinished()]:
+                vehicle.setFinished()
+            for vehicle in [vehicle for vehicle in vehicles if vehicle.isFinished()]:
+                vehicle.print()
+                #vehicles.remove(vehicle)
+
+            # add centroids to vehicles, create new vehicles or dismiss centroids
+            if lastCentroids and currentCentroids:
+                addCentroidsToVehicles()
+
+            # check if vehicles crossed the line
+            cv2.line(image, (VehicleInfo.MIDDLE_X, 300), (VehicleInfo.MIDDLE_X, 1000), VehicleInfo.RED)
+            detectVehicles()
+
+            # draw vehicle lines:
+            for vehicle in [vehicle for vehicle in vehicles if not vehicle.isFinished()]:
+                vehicle.drawVehicleLine(image)
+
             # show windows:
             cv2.namedWindow("Main", cv2.WINDOW_NORMAL)
             cv2.resizeWindow("Main", WINDOW_SIZE, WINDOW_SIZE)
@@ -64,75 +81,55 @@ def main():
     cv2.destroyAllWindows()
 
 
-def getForeground(image):
+def getForeground(backSubtractor):
     gray = np.float32(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
-    noisyForeground = back_sub.apply(gray, None, LEARNING_RATE)
+    noisyForeground = backSubtractor.apply(gray, None, LEARNING_RATE)
     cleanForeground = cv2.morphologyEx(noisyForeground, cv2.MORPH_OPEN, KERNEL)
     return cleanForeground
 
 
-def detectVehicles(image, foreground):
-    global count, found, last_centroid, current_centroid
-    if current_centroid:
-        last_centroid = current_centroid
-    current_centroid = getCentroid(foreground)
-    if last_centroid:
-        identify_current_centroid()
-    if vehicles and not vehicles[-1].getFound():
-        first_x = vehicles[-1].get_centroid(0)[0]
-        last_x = vehicles[-1].get_centroid(-1)[0]
-        if first_x > last_x:
-            x2 = first_x
-            x1 = last_x
+def getCurrentCentroids(foreground):
+    centroids = []
+    _, contours, _ = cv2.findContours(foreground, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for pointSet in contours:
+        x, y, width, height = cv2.boundingRect(pointSet)
+        if width >= MIN_RECTANGLE_SIZE and height >= MIN_RECTANGLE_SIZE:
+            centroids.append(Centroid(x + int(width / 2), y + int(height / 2)))
+    return centroids
+
+
+def addCentroidsToVehicles():
+    # add current centroids to current, not finished vehicles:
+    for current in currentCentroids:
+        for vehicle in vehicles:
+            if current.isNearBy(vehicle.getCentroid(-1)):
+                vehicle.addCentroid(current)
+                currentCentroids.remove(current)
+                break
+    # add new vehicle:
+    for current in currentCentroids:
+        for last in lastCentroids:
+            if current.isNearBy(last):
+                vehicles.append(Vehicle(last, current))
+
+
+def detectVehicles():
+    global count, found
+    for vehicle in [vehicle for vehicle in vehicles if not vehicle.isFound()]:
+        startX = vehicle.getCentroid(0).getX()
+        endX = vehicle.getCentroid(-1).getX()
+        if startX > endX:
+            order = -1
         else:
-            x1 = first_x
-            x2 = last_x
-        for x in range(x1, x2):
-            if x == LINE_X:
+            order = 1
+        for x in range(startX, endX, order):
+            if x == VehicleInfo.MIDDLE_X:
                 count += 1
                 print("Vehicle found:", count)
-                cv2.circle(image, current_centroid, CIRCLE_RADIUS + 10, GREEN, THICKNESS)
+                cv2.circle(image, vehicle.getCentroid(-1).getXY(), 10, VehicleInfo.GREEN, -1)
                 found = True
-                vehicles[-1].setFound(True)
+                vehicle.setFound(True)
                 break
-
-
-def getCentroid(foreground):
-    centroidX, centroidY = 0, 0
-    img, contours, hierarchy = cv2.findContours(foreground, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # each point_set may vary in size
-    for point_set in contours:
-        x, y, width, height = cv2.boundingRect(point_set)
-        # make sure the resulting rectangle is large enough
-        # for instance, there may be small point_sets resulting in too small rectangles
-        # meaning an object has not been detected there
-        if (width >= MIN_RECTANGLE_SIZE) and (height >= MIN_RECTANGLE_SIZE):
-            centroidX = x + int(width / 2)
-            centroidY = y + int(height / 2)
-    return centroidX, centroidY
-
-
-def identify_current_centroid():
-    if current_centroid_nearby(last_centroid[0], last_centroid[1]):
-        if not vehicles:
-            # first vehicle:
-            vehicles.append(Vehicle(current_centroid))
-        else:
-            last_vehicle_centroid = vehicles[-1].get_centroid(-1)
-            if current_centroid_nearby(last_vehicle_centroid[0], last_vehicle_centroid[1]):
-                # current vehicle:
-                vehicles[-1].add_centroid(current_centroid)
-            else:
-                # new vehicle:
-                vehicles.append(Vehicle(current_centroid))
-
-
-def current_centroid_nearby(x, y):
-    if (x - X_DEVIATION) < current_centroid[0] < (x + X_DEVIATION) and \
-                            (y - Y_DEVIATION) < current_centroid[1] < (y + Y_DEVIATION):
-        return True
-    else:
-        return False
 
 
 if __name__ == "__main__":
--- workspace/Vehicle.py
+++ workspace/Vehicle.py
@@ -1,27 +1,81 @@
 import cv2
 
+from VehicleInfo import WHITE, STEP, MIDDLE_X, font
+
 
 class Vehicle:
-    def __init__(self, centroid):
+    num = 1
+
+    def __init__(self, lastCentroid, currentCentroid):
         self.found = False
+        self.finished = False
+        self.direction = None
         self.centroids = []
-        self.centroids.append(centroid)
+        self.centroids.append(lastCentroid)
+        self.centroids.append(currentCentroid)
+        self.name = "Vehicle" + str(Vehicle.num)
+        Vehicle.num += 1
+
+    def drawVehicleLine(self, image):
+        centroid_num = 1
+        title = self.name + "(" + str(len(self.centroids)) + ")"
+        cv2.putText(image, title, (self.centroids[0].getX() - 100, self.centroids[0].getY() - 20), font, 1, WHITE, 2,
+                    cv2.LINE_AA)
+        for index, centroid in enumerate(self.centroids[:-1]):
+            cv2.line(image, centroid.getXY(), self.centroids[index + 1].getXY(), WHITE)
+        for centroid in self.centroids:
+            cv2.circle(image, centroid.getXY(), 5, WHITE, -1)
+            cv2.putText(image, str(centroid_num), centroid.getXY(), font, 1, WHITE, 1, cv2.LINE_AA)
+            centroid_num += 1
+
+    def setDirection(self):
+        eastCount, westCount = 0, 0
+        if len(self.centroids) > 3:
+            for index, centroid in enumerate(self.centroids[:-1]):
+                nextCentroid = self.centroids[index + 1]
+                if nextCentroid.getX() - STEP > centroid.getX():
+                    eastCount += 1
+                elif nextCentroid.getX() + STEP < centroid.getX():
+                    westCount += 1
+            if eastCount > westCount:
+                self.direction = "east"
+            elif eastCount < westCount:
+                self.direction = "west"
+
+    def getDirection(self):
+        return self.direction
+
+    def setFinished(self):
+        if (self.direction == "west" and self.centroids[-1].getX() < MIDDLE_X - 300) or (
+                        self.direction == "east" and self.centroids[-1].getX() > MIDDLE_X + 300):
+            self.finished = True
+
+    def isFinished(self):
+        return self.finished
 
     def setFound(self, value):
         self.found = value
 
-    def getFound(self):
+    def isFound(self):
         return self.found
 
-    def add_centroid(self, centroid):
+    def addCentroid(self, centroid):
         self.centroids.append(centroid)
 
-    def get_centroid(self, index):
+    def getCentroid(self, index):
         return self.centroids[index]
 
-    def get_all_centroids(self):
+    def getAllCentroids(self):
         return self.centroids
 
-    def draw_vehicle_line(self, image, color):
-        for i in range(len(self.centroids) - 1):
-            cv2.line(image, self.centroids[i], self.centroids[i + 1], color)
+    def print(self):
+        print(self.name.upper() + ": ", end="")
+        print("direction:", self.direction, end=", ")
+        print("found:", self.found, end=", ")
+        print("finished:", self.finished)
+        for centroid in self.centroids:
+            print(centroid.getXY(), end=" ")
+        print("\n\n")
+
+    def getName(self):
+        return self.name
