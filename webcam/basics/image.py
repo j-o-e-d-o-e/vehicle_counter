a527d2c967d485b6efbb281e358bea14e80bc538
joedoe <d_joe@gmx.net>
2018-01-21 19:05:50 +0100

    project structure adjusted

--- webcam/basics/image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-img = cv2.imread("../../img/image.jpg", cv2.IMREAD_GRAYSCALE)
-
-plt.imshow(img, cmap="gray",interpolation="bicubic")
-plt.plot([100,120],[120,120],"b", linewidth=12)
-plt.show()
