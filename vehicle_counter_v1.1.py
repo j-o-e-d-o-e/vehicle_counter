ccd722c54260472306d8ec14350df4f1bf7ef093
j-o-e-d-o-e <d_joe@gmx.net>
2018-01-27 02:51:54 +0100

    aux_merge_contours() added

--- vehicle_counter_v1.1.py
+++ vehicle_counter_v1.1.py
@@ -4,30 +4,38 @@ import cv2
 import time
 import uuid
 import numpy as np
+import csv
 
 # Lower -> smaller changes are more readily detected
-THRESHOLD_SENSITIVITY = 50  # default: 50
-# The number of square pixels a contour must be before considering it a candidate for tracking
-CONTOUR_SIZE = 500  # default: 500
-# Higher -> contours consist of less points -> less computation
-EPSILON = 0.01  # default: 0.01
-# The maximum distance between vehicle and centroid to connect (in px)
-LOCKON_DISTANCE = 120  # default: 80
+THRESHOLD = 50
+# The number of square pixels a contour must have to be considered a candidate for tracking
+CONTOUR_SIZE = 500
+# Higher -> contours consist of less points
+EPSILON = 0.05
+# The maximum distance between the points of two contours to be considered close
+CONTOUR_X_DISTANCE = 300
+CONTOUR_Y_DISTANCE = 20
+# The maximum distance between vehicle and centroid to connect
+LOCKON_DISTANCE = 120
 # The minimum distance between an existing vehicle and a new vehicle
-VEHICLE_DISTANCE = 350  # default: 350
+VEHICLE_DISTANCE = 350
 # To filter instantaneous changes from the frame
 KERNEL = (21, 21)
-# How much the current frame impacts the average frame (higher -> more change and smaller differences)
-AVERAGE_WEIGHT = 0.04  # default: 0.04
+# How much the current frame impacts the average frame
+AVERAGE_WEIGHT = 0.04
 # How long a vehicle is allowed to sit around without having any new centroid
-VEHICLE_TIMEOUT = 0.8  # default: 0.7
+VEHICLE_TIMEOUT = 0.8
 # Center on the x axis for the center line
-X_CENTER = 450  # default: 400 (precisely, 384)
+X_CENTER = 400
+# Center on the y axis which separates the two lanes
+Y_CENTER = 250
 # Constants for drawing on the frame
 BLUE = (255, 0, 0)
 GREEN = (0, 255, 0)
 RED = (0, 0, 255)
 WHITE = (255, 255, 255)
+# A variable for the path of the video file
+PATH_VIDEO = '../vehicle_counter_desktop/videos/rec1_16s.h264'
 
 # A variable to store the video capture or the pi camera
 cam = None
@@ -41,16 +49,22 @@ current_centroids = []
 last_centroids = []
 # A list of tracked vehicles
 vehicles = []
-# A variable to store the counted vehicles
-count = 0
+# A variable to store the vehicle count
+vehicle_counter = 0
 # A variable to sleep for 3 secs if vehicle is found
 found = False
 # A variable to pause/unpause frame processing
 pause = False
 
+# For saving data to .csv-file
+file = open('../../../../Desktop/vehicle_counter.csv', 'w', newline='')
+fields = ['id', 'first_seen', 'last_seen', 'dir', 'found', 'track']
+csv_writer = csv.DictWriter(file, fieldnames=fields)
+csv_writer.writeheader()
+
 
 def main_pi():
-    global cam, found
+    global cam
     cam = PiCamera()
     cam.framerate = 32
     cam.resolution = (640, 480)
@@ -67,12 +81,9 @@ def main_pi():
         main_loop(frame)
         raw_capture.truncate(0)
 
-    cv2.destroyAllWindows()
-    print("Vehicles found total:", count)
-
 
 def main_cam():
-    global cam, pause, found
+    global cam
     cam = cv2.VideoCapture(0)
     while True:
         if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -87,13 +98,11 @@ def main_cam():
         main_loop(frame)
 
     cam.release()
-    cv2.destroyAllWindows()
-    print("Vehicles found total:", count)
 
 
 def main_vid():
     global cam, pause, found
-    cam = cv2.VideoCapture('C:/Users/joe/Documents/programming/py/open_cv_desktop/videos/rec2_32s.h264')
+    cam = cv2.VideoCapture(PATH_VIDEO)
     while True:
         if cv2.waitKey(1) & 0xFF == ord("p"):
             if pause:
@@ -119,12 +128,10 @@ def main_vid():
         main_loop(frame)
 
     cam.release()
-    cv2.destroyAllWindows()
-    print("Vehicles found total:", count)
 
 
 def main_loop(frame):
-    global last_centroids, current_centroids, vehicles, frame_time
+    global frame_time, last_centroids, current_centroids
     frame_time = time.time()
     processed_frame = process_frame(frame)
     contours = get_contours(frame, processed_frame)
@@ -135,9 +142,8 @@ def main_loop(frame):
         last_centroids = current_centroids = []
     if last_centroids and current_centroids:
         add_centroids_to_vehicles()
-
     cv2.line(frame, (X_CENTER, 100), (X_CENTER, 400), RED)
-
+    cv2.line(frame, (100, Y_CENTER), (800, Y_CENTER), RED)
     if vehicles:
         detect_vehicles(frame)
     debug(frame)
@@ -154,71 +160,63 @@ def process_frame(frame):
     cv2.accumulateWeighted(gray_frame, avg_frame, AVERAGE_WEIGHT)
 
     difference_frame = cv2.absdiff(gray_frame, cv2.convertScaleAbs(avg_frame))
+    # cv2.imshow("1 - DIFFERENCE between the average and current frame", difference_frame)
 
-    _, threshold_frame = cv2.threshold(difference_frame, THRESHOLD_SENSITIVITY, 255, cv2.THRESH_BINARY)
+    _, threshold_frame = cv2.threshold(difference_frame, THRESHOLD, 255, cv2.THRESH_BINARY)
     threshold_frame = cv2.dilate(threshold_frame, None, iterations=2)
+    # cv2.imshow("2 - THRESHOLD with clean shapes of the moving objects", threshold_frame)
     return threshold_frame
+
+
+def get_contours(frame, processed_frame):
+    global pause
+    _, contours, _ = cv2.findContours(processed_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = filter(lambda c: cv2.contourArea(c) > CONTOUR_SIZE, contours)
+    contours = [cv2.approxPolyDP(contour, EPSILON * cv2.arcLength(contour, True), True) for contour in contours]
+
+    for i, contour in enumerate(contours):
+        cv2.drawContours(frame, [contour], -1, WHITE)
+        text = "Contour " + str(i)
+        cv2.putText(frame, text, (contour[0][0][0], contour[0][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 1,
+                    cv2.LINE_AA)
+
+    if contours:
+        contours = aux_merge_contours(contours, len(contours) - 1)
+        contours = sorted(contours, key=lambda c: cv2.contourArea(c), reverse=True)
+        cv2.drawContours(frame, contours, -1, GREEN, 2)
+    return contours
+
+
+def aux_merge_contours(contours, iterations):
+    count = 0
+    while count < iterations:
+        clip_board = []
+        is_added = [False] * len(contours)
+        for i, contour_1 in enumerate(contours[:-1]):
+            if aux_close(contour_1, contours[i + 1]):
+                merged_contour = np.vstack((contour_1, contours[i + 1]))
+                merged_contour = cv2.convexHull(merged_contour)
+                clip_board.append(merged_contour)
+            else:
+                if not is_added[i]:
+                    clip_board.append(contour_1)
+                    is_added[i] = True
+                if i == len(contours) - 2:
+                    clip_board.append(contours[-1])
+        contours = clip_board.copy()
+        count += 1
+    return contours
 
 
 def aux_close(contour_1, contour_2):
     for point_1 in contour_1:
         for point_2 in contour_2:
-            distance_x = abs(point_1[0][0] - point_2[0][0])
-            distance_y = abs(point_1[0][1] - point_2[0][1])
-            if distance_x < 300 and distance_y < 20:
-                return True
+            if not (point_1[0][1] < Y_CENTER < point_2[0][1] or point_1[0][1] > Y_CENTER > point_2[0][1]):
+                distance_x = abs(point_1[0][0] - point_2[0][0])
+                distance_y = abs(point_1[0][1] - point_2[0][1])
+                if distance_x < CONTOUR_X_DISTANCE and distance_y < CONTOUR_Y_DISTANCE:
+                    return True
     return False
-
-
-def get_contours(frame, processed_frame):
-    _, contours, _ = cv2.findContours(processed_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = filter(lambda c: cv2.moments(c)['m00'] > CONTOUR_SIZE, contours)
-    contours = [cv2.approxPolyDP(contour, EPSILON * cv2.arcLength(contour, True), True) for contour in contours]
-    contours = sorted(contours, key=lambda c: cv2.contourArea(c), reverse=True)
-    
-    if contours:
-        groups = [[] for l in range(len(contours) - 1)]
-        for i, contour_1 in enumerate(contours[:-1]):
-            for j, contour_2 in enumerate(contours[i + 1:]):
-                if aux_close(contour_1, contour_2):
-                    groups[i].append(i + j + 1)
-
-        group_indices = [0] * len(contours)
-        change_holder = [True] + [False] * (len(contours) - 1)
-        group_num = 0
-        for i, group_1 in enumerate(groups):
-            if change_holder[i]:
-                index = group_indices[i]
-            else:
-                index = group_num
-                group_indices[i] = index
-                change_holder[i] = True
-            if group_1 == []:
-                group_num = index + 1
-            for j in group_1:
-                group_indices[j] = index
-                change_holder[j] = True
-            for group_2 in groups[i + 1:]:
-                if any(k in group_1 for k in group_2):
-                    for l in group_2:
-                        group_indices[l] = index
-                        change_holder[l] = True
-                else:
-                    group_num = index + 1
-        if not change_holder[-1]:
-            group_indices[len(change_holder) - 1] = group_num
-
-        merged_contours = []
-        for i in range(max(group_indices) + 1):
-            contour_indices = [j for j, v in enumerate(group_indices) if v == i]
-            if contour_indices:
-                contour = np.vstack(contours[i] for i in contour_indices)
-                merged_contour = cv2.convexHull(contour)
-                merged_contours.append(merged_contour)
-
-        merged_contours = sorted(merged_contours, key=lambda c: cv2.contourArea(c), reverse=True)
-        cv2.drawContours(frame, merged_contours, -1, GREEN, 2)
-        return merged_contours
 
 
 def get_centroids(frame, contours):
@@ -227,34 +225,30 @@ def get_centroids(frame, contours):
         x, y, width, height = cv2.boundingRect(contour)
         cx = int(x + width / 2)
         cy = int(y + height / 2)
-        # better to use boundinRect instead of moments to avoid zero-division
-        # moments = cv2.moments(contour)
-        # cx = int(moments['m10'] / moments['m00'])
-        # cy = int(moments['m01'] / moments['m00'])
         cv2.circle(frame, (cx, cy), 10, RED, -1)
         centroids.append((cx, cy))
     return centroids
 
 
 def add_centroids_to_vehicles():
-    centroids = current_centroids.copy()
+    candidates = current_centroids.copy()
     if vehicles:
         for vehicle in vehicles:
-            for current in centroids:
-                distance = cv2.norm(current, vehicle['track'][0])
+            for centroid in candidates:
+                distance = cv2.norm(centroid, vehicle['track'][0])
                 if distance < LOCKON_DISTANCE:
-                    if (vehicle['dir'] == 'left' and vehicle['track'][0][0] > current[0]) or (
-                                    vehicle['dir'] == 'right' and vehicle['track'][0][0] < current[0]):
-                        vehicle['track'].insert(0, current)
+                    if (vehicle['dir'] == 'left' and vehicle['track'][0][0] > centroid[0]) or (
+                                    vehicle['dir'] == 'right' and vehicle['track'][0][0] < centroid[0]):
+                        vehicle['track'].insert(0, centroid)
                         vehicle['last_seen'] = frame_time
-                        centroids.remove(current)
-        if centroids:
-            for current in centroids:
-                if all(cv2.norm(current, vehicle['track'][0]) > VEHICLE_DISTANCE for vehicle in vehicles):
-                    add_new_vehicle(current)
+                        candidates.remove(centroid)
+        if candidates:
+            for centroid in candidates:
+                if all(cv2.norm(centroid, vehicle['track'][0]) > VEHICLE_DISTANCE for vehicle in vehicles):
+                    add_new_vehicle(centroid)
     else:
-        for current in current_centroids:
-            add_new_vehicle(current)
+        for centroid in current_centroids:
+            add_new_vehicle(centroid)
 
 
 def add_new_vehicle(current):
@@ -278,10 +272,12 @@ def add_new_vehicle(current):
 
 
 def detect_vehicles(frame):
-    global count, found
+    global vehicle_counter, found
     for i in range(len(vehicles) - 1, -1, -1):
         if frame_time - vehicles[i]['last_seen'] > VEHICLE_TIMEOUT:
             print("Removing expired vehicle {}".format(vehicles[i]['id']))
+            if vehicles[i]['found']:
+                csv_writer.writerow(vehicles[i])
             del vehicles[i]
 
     for vehicle in [v for v in vehicles if not v['found']]:
@@ -293,8 +289,8 @@ def detect_vehicles(frame):
             order = 1
         for x in range(start_x, end_x, order):
             if x == X_CENTER:
-                count += 1
-                print("Vehicles found:", count)
+                vehicle_counter += 1
+                print("Vehicles found:", vehicle_counter)
                 cv2.circle(frame, vehicle['track'][0], 10, GREEN, -1)
                 found = True
                 vehicle['found'] = True
@@ -304,9 +300,14 @@ def detect_vehicles(frame):
 def debug(frame):
     for vehicle in vehicles:
         cv2.line(frame, vehicle['track'][-1], vehicle['track'][0], RED)
+        for centroid in vehicle['track']:
+            cv2.circle(frame, centroid, 5, BLUE, -1)
 
-    cv2.imshow("4 - PREVIEW containing information about tracked vehicles", frame)
+    cv2.imshow("4 - PREVIEW with information about tracked vehicles", frame)
 
 
 if __name__ == "__main__":
-    main_cam()
+    main_vid()
+    cv2.destroyAllWindows()
+    file.close()
+    print("Vehicles found total:", vehicle_counter)
